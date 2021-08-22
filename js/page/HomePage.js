c02717ee672edc96ad1a86ae9cdaf46204b1c5ca
wangxinyue <1977795940@qq.com>
2021-08-22 16:42:20 +0800

    修复启动报错，添加底部navigation

--- js/page/HomePage.js
+++ js/page/HomePage.js
@@ -1,13 +1,73 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createAppContainer} from 'react-navigation';
+import PopularPage from '../page/PopularPage';
+import TrendingPage from '../page/TrendingPage';
+import MyPage from '../page/MyPage';
+import FavoritePage from '../page/FavoritePage';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 export default class HomePage extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>HomePage</Text>
-      </View>
+  _tabNavigator() {
+    return createAppContainer(
+      createBottomTabNavigator({
+        PopularPage: {
+          screen: PopularPage,
+          navigationOptions: {
+            tabBarLabel: '最热',
+            tabBarIcon: ({tintColor, focused}) => (
+              <MaterialIcons
+                name={'whatshot'}
+                size={26}
+                style={{color: tintColor}}
+              />
+            ),
+          },
+        },
+        TrendingPage: {
+          screen: TrendingPage,
+          navigationOptions: {
+            tabBarLabel: '趋势',
+            tabBarIcon: ({tintColor, focused}) => (
+              <Ionicons
+                name={'md-trending-up'}
+                size={26}
+                style={{color: tintColor}}
+              />
+            ),
+          },
+        },
+        MyPage: {
+          screen: MyPage,
+          navigationOptions: {
+            tabBarLabel: '我的',
+            tabBarIcon: ({tintColor, focused}) => (
+              <Entypo name={'user'} size={26} style={{color: tintColor}} />
+            ),
+          },
+        },
+        FavoritePage: {
+          screen: FavoritePage,
+          navigationOptions: {
+            tabBarLabel: '收藏',
+            tabBarIcon: ({tintColor, focused}) => (
+              <MaterialIcons
+                name={'favorite'}
+                size={26}
+                style={{color: tintColor}}
+              />
+            ),
+          },
+        },
+      }),
     );
+  }
+  render() {
+    const Tab = this._tabNavigator()
+    return <Tab/>;
   }
 }
 
